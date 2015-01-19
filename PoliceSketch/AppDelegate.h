cbb0253cf188d37e5ed4b66e4310b08fa46fe18e
manuntag <manuntag@ualberta.ca>
2015-01-19 10:13:13 -0800

    Initial Commit

---
+++ PoliceSketch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PoliceSketch
+//
+//  Created by David Manuntag on 2015-01-19.
+//  Copyright (c) 2015 David Manuntag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PoliceSketchTests/PoliceSketchTests.m
@@ -0,0 +1,40 @@
+//
+//  PoliceSketchTests.m
+//  PoliceSketchTests
+//
+//  Created by David Manuntag on 2015-01-19.
+//  Copyright (c) 2015 David Manuntag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface PoliceSketchTests : XCTestCase
+
+@end
+
+@implementation PoliceSketchTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
